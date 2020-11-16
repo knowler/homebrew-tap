258329721a5d8b47b56bafba4037719ebc536163
Nathan Knowler <nathan@knowler.me>
2020-11-15 23:32:04 -0600

    Add Alacritty 0.6.0 release candidate 2
    Probably will be released soon

---
+++ Casks/alacritty.rb
@@ -0,0 +1,17 @@
+cask "alacritty" do
+  version "0.6.0-rc2"
+  sha256 "b07faec934394c9e898be5a1d1d833f450f31e1b575edc1cb55a872cb9242f67"
+
+  url "https://github.com/alacritty/alacritty/releases/download/v#{version}/Alacritty-v#{version}.dmg"
+  appcast "https://github.com/alacritty/alacritty/releases.atom"
+  name "Alacritty"
+  desc "Cross-platform, GPU-accelerated terminal emulator"
+  homepage "https://github.com/alacritty/alacritty"
+
+  app "Alacritty.app"
+  binary "#{appdir}/Alacritty.app/Contents/MacOS/alacritty"
+
+  zap delete: [
+    "~/Library/Saved Application State/io.alacritty.savedState",
+  ]
+end
