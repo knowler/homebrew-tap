3933f51de6fbb698771a80b130ac4247a588db05
Nathan Knowler <nathan@knowler.me>
2020-11-22 22:53:38 -0600

    Update Alacritty from 0.6.0-rc2 to 0.6.0-rc3

--- Casks/alacritty.rb
+++ Casks/alacritty.rb
@@ -1,6 +1,6 @@
 cask "alacritty" do
-  version "0.6.0-rc2"
-  sha256 "b07faec934394c9e898be5a1d1d833f450f31e1b575edc1cb55a872cb9242f67"
+  version "0.6.0-rc3"
+  sha256 "988516c263a3a03648fa8b81fa175a72b25b58d958318084c712e9dc28618907"
 
   url "https://github.com/alacritty/alacritty/releases/download/v#{version}/Alacritty-v#{version}.dmg"
   appcast "https://github.com/alacritty/alacritty/releases.atom"
